5c15879f61720b0c96b365fccabba918694ea482
Kyle Laverty <klaverty@walkwest.com>
2022-04-06 09:43:29 -0400

    Archive template updates and style adjustments for posts

--- archive.php
+++ archive.php
@@ -21,21 +21,27 @@ get_header();
 				?>
 			</header><!-- .page-header -->
 			
-			<div class="row">
+			<div class="row the-posts">
 				<?php
 				/* Start the Loop */
 				while ( have_posts() ) :
 					the_post(); ?>
 			
-					<div class="col-md-4">
-						<?php  ?>
+					<div class="col-md-4 the-post">
 						<?php the_post_thumbnail(); ?>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						<?php the_excerpt(); ?>
-					</div>
+						<a href="<?php the_permalink(); ?>" class="the-post-title"><?php the_title(); ?></a>
+						<div class="the-post-content"><?php the_excerpt(); ?></div>
+						<?php if( get_field( 'start_date' ) ) : ?>
+							
+							<span class="date start-date"><em>Event Start: <?php echo get_field('start_date'); ?></em></span>
+							<span class="date end-date"><em>Event End: <?php echo get_field('end_date'); ?></em></span>
+							
+						<?php endif; ?>
+						<a href="<?php the_permalink(); ?>" class="btn btn-primary the-post-btn">Learn More</a>
+					</div><!-- .col-md-4 .the-post -->
 			
 				<?php endwhile; ?>
-			</div>
+			</div><!-- .row .the-posts -->
 		
 			<?php the_posts_navigation();
 		
--- taxonomy-event_type-webinar.php
+++ taxonomy-event_type-webinar.php
@@ -42,7 +42,7 @@ get_header();
 		$query = new WP_Query( $args );
 		
 		// The Loop
-		if ( $query->have_posts() ) { ?>
+		if ( $query->have_posts() ) : ?>
 		
 			<header class="page-header jumbotron text-center">
 				<?php
@@ -51,22 +51,33 @@ get_header();
 				?>
 			</header><!-- .page-header -->
 		
-			<div class="row">
-			<?php while ( $query->have_posts() ) {
-				$query->the_post(); ?>
+			<div class="row the-posts">
+				<?php
+				/* Start the Loop */
+				while ( $query->have_posts() ) :
+					$query->the_post(); ?>
+					
+					<div class="col-md-4 the-post">
+						<?php the_post_thumbnail(); ?>
+						<a href="<?php the_permalink(); ?>" class="the-post-title"><?php the_title(); ?></a>
+						<div class="the-post-content"><?php the_excerpt(); ?></div>
+						
+						<?php if( get_field( 'start_date' ) ) : ?>
+							
+							<span class="date start-date"><em>Event Start: <?php echo get_field('start_date'); ?></em></span>
+							<span class="date end-date"><em>Event End: <?php echo get_field('end_date'); ?></em></span>
+							
+						<?php endif; ?>
+						
+						<a href="<?php the_permalink(); ?>" class="btn btn-primary the-post-btn">Learn More</a>
+					</div><!-- .col-md-4 .the-post -->
+					
+				<?php endwhile; ?>
+			</div><!-- .row .the-posts -->
 				
-				<div class="col-md-4">
-					<?php  ?>
-					<?php the_post_thumbnail(); ?>
-					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					<?php the_excerpt(); ?>
-				</div>
-				
-			<?php } ?>
-			</div>
-		<?php } else {
-			// no posts found
-		}
+			<?php the_posts_navigation();
+			
+		endif;
 		
 		// Restore original Post Data
 		wp_reset_postdata();
